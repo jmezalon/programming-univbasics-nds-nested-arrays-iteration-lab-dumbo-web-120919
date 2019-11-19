0ec4c024a2d3add4c8475860acb378c1c1eb47c5
Jean Max Mezalon <jeanmaxmezalon@pursuit.org>
2019-11-19 13:56:27 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -28,7 +28,7 @@ def find_greater_pair(src)
   while row_index < src.count do 
     element_index = 0
     while element_index < src[row_index].count do 
-      if src[row_index][element_index] > src[row_index][element_index+1]
+      if src[row_index][element_index] > src[row_index][element_index++]
         res << src[row_index][element_index]
       end
       element_index += 1 
